98f73c32dd122117182fad8feb60dacfd9f440a3
Greg <gregorydrummond@ufl.edu>
2021-06-29 15:43:37 -0400

    Logout Functionality

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,11 +1,19 @@
 package com.codepath.apps.restclienttemplate;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.codepath.apps.restclienttemplate.adapters.TweetsAdapter;
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -27,6 +35,7 @@ public class TimelineActivity extends AppCompatActivity {
     RecyclerView rvTweets;
     List<Tweet> tweets;
     TweetsAdapter adapter;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,10 +57,28 @@ public class TimelineActivity extends AppCompatActivity {
 
         populateHomeTimeLine();
 
+        //Set TOOLBAR
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Twitter");
+        toolbar.inflateMenu(R.menu.login);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if(item.getItemId() == R.id.action_logout) {
+                    // forget who's logged in
+                    client.clearAccessToken();
+                    // navigate backwards to Login screen
+                    finish();
+                }
+                return false;
+            }
+        });
+
     }
 
     private void populateHomeTimeLine() {
         client.getHomeTimeline(new JsonHttpResponseHandler() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
                 Log.i(TAG, "Timeline fetch successful: " + json.toString());
@@ -70,4 +97,20 @@ public class TimelineActivity extends AppCompatActivity {
             }
         });
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.login, menu);
+//        //Get menu item
+//        MenuItem logoutItem = menu.findItem(R.id.action_logout);
+//        final MenuView logoutView = (MenuView) MenuItemCompat.getActionProvider(logoutItem);
+//
+//
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//
+//    public void onClick(View view) {
+//        finish();
+//    }
 }
